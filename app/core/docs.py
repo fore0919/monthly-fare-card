f9af9d8121a0154abb9a4597f86b0fdccc5d1b29
fore0919 <gms02294@gmail.com>
2024-06-26 18:01:03 +0900

    fix: docs

--- app/core/docs.py
+++ app/core/docs.py
@@ -1,9 +1,12 @@
 description = """
 ## 교통 카드 추천 API
-github_link : https://github.com/fore0919/monthly-fare-card
+`description`: 사용자가 생년월일과 출발지/도착지 (지하철역, 버스정류장)을 입력하면 평일기준 왕복 출퇴근시 예상 교통비를 계산하는 기능을 가진 product 입니다.
+</br>
+</br>
+`github_link` : [https://github.com/fore0919/monthly-fare-card](https://github.com/fore0919/monthly-fare-card)
 """
 fare_card = """
-### 평균 한달 출근 시 교통카드 할인액을 계산하는 API
+### 평균 한달 출근 시 교통카드 할인금액을 계산하는 API
 
 - 주말, 국경일, 대체공휴일은 제외한 평일만 계산 (공공데이터API 이용)
 - 왕복 기준

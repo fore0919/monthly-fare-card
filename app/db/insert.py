c265e99cae17cf43102dd0881d38e3b6970c4f26
fore0919 <gms02294@gmail.com>
2025-02-27 21:01:29 +0900

    fix: temp

--- app/db/insert.py
+++ app/db/insert.py
@@ -2,12 +2,22 @@ import csv
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.models.station import Line, Station
+
 
 async def bulk_insert_station_data(
     session: AsyncSession, file_path: str
 ) -> None:
     with open(file=file_path, mode="r", encoding="utf-8") as file:
         reader = csv.DictReader(file)
-        stations = [Station(id=int(row["id"])) for row in reader]
+        stations = [Station(station_name=str(row["name"])) for row in reader]
+        session.bulk_save_objects(stations)
+    await session.commit
+
+
+async def bulk_insert_line_data(session: AsyncSession, file_path: str) -> None:
+    with open(file=file_path, mode="r", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+        stations = [Line(line_name=str(row["lines"])) for row in reader]
         session.bulk_save_objects(stations)
     await session.commit
--- app/models/station.py
+++ app/models/station.py
@@ -1,81 +1,49 @@
-from typing import TYPE_CHECKING, Any, Optional
-
-from sqlalchemy import ForeignKey
+from sqlalchemy import DECIMAL, Boolean, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from app.db.base_model import Base, TimeStamps
-
-if TYPE_CHECKING:
-    from app.models import Location
+from app.db.base_model import Base
 
 
-class Card(Base, TimeStamps):
-    __tablename__ = "cards"
+class Station(Base):
+    __tablename__ = "stations"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(nullable=False)
-    max_count: Mapped[int] = mapped_column()
-    min_count: Mapped[int] = mapped_column()
-    youth_age: Mapped[int] = mapped_column()
-    senior_age: Mapped[int] = mapped_column()
-    min_cost: Mapped[int] = mapped_column(nullable=False, default=0)
-    adult: Mapped[int] = mapped_column()
-    youth: Mapped[int] = mapped_column()
-    low: Mapped[int] = mapped_column()
-    senior: Mapped[int] = mapped_column()
-    location_id: Mapped[int] = mapped_column(ForeignKey("locations.id"))
-    location: Mapped["Location"] = relationship(foreign_keys=[location_id])
+    station_name: Mapped[str] = mapped_column(nullable=False)
+    is_departure_allowed: Mapped[bool] = mapped_column(
+        Boolean, nullable=False, default=False
+    )
+    is_arrival_allowed: Mapped[bool] = mapped_column(
+        Boolean, nullable=False, default=False
+    )
+    station_lines: Mapped[list["StationLine"]] = relationship(
+        "StationLine", back_populates="station"
+    )
 
-    def __init__(
-        self,
-        id: int,
-        name: str,
-        max_count: Optional[int],
-        min_count: int,
-        youth_age: int,
-        senior_age: int,
-        min_cost: int,
-        adult: int,
-        youth: int,
-        low: int,
-        senior: Optional[int],
-        location_id: Optional[int],
-    ):
-        self.id = id
-        self.name = name
-        self.max_count = max_count
-        self.min_count = min_count
-        self.youth_age = youth_age
-        self.senior_age = senior_age
-        self.min_cost = min_cost
-        self.adult = adult
-        self.youth = youth
-        self.low = low
-        self.senior = senior
-        self.location_id = location_id
 
-    def __repr__(self):
-        return (
-            f"Card(id={self.id}, name={self.name}, max_count={self.max_count}, "
-            f"min_count={self.min_count}, youth_age={self.youth_age}, "
-            f"senior_age={self.senior_age}, min_cost={self.min_cost}, "
-            f"adult={self.adult}, youth={self.youth}, low={self.low}, senior={self.senior}"
-            f"location_id={self.location_id})"
-        )
+class Line(Base):
+    __tablename__ = "lines"
 
-    @classmethod
-    def from_json(cls, data: dict[str, Any]) -> "Card":
-        return cls(
-            id=data["id"],
-            name=data["name"],
-            max_count=data.get("max_count"),
-            min_count=data["min_count"],
-            youth_age=data["youth_age"],
-            senior_age=data["senior_age"],
-            min_cost=data["min_cost"],
-            adult=data["adult"],
-            youth=data["youth"],
-            low=data["low"],
-            senior=data["senior"],
-            location_id=data.get("location_id"),
-        )
+    id: Mapped[int] = mapped_column(primary_key=True)
+    line_name: Mapped[str] = mapped_column(nullable=False)
+    operator: Mapped[str] = mapped_column(nullable=False)
+    station_lines: Mapped[list["StationLine"]] = relationship(
+        "StationLine", back_populates="line"
+    )
+
+
+class StationLine(Base):
+    __tablename__ = "station_line"
+
+    id: Mapped[int] = mapped_column("station_line_id", primary_key=True)
+    station_id: Mapped[int] = mapped_column(
+        ForeignKey("station.station_id"), nullable=False
+    )
+    line_id: Mapped[int] = mapped_column(
+        ForeignKey("line.line_id"), nullable=False
+    )
+    sequence: Mapped[int] = mapped_column(nullable=False)
+    distance_from_prev: Mapped[float] = mapped_column(
+        DECIMAL(5, 2), nullable=True
+    )
+    station: Mapped["Station"] = relationship(back_populates="station_lines")
+    line: Mapped["Line"] = relationship(back_populates="station_lines")

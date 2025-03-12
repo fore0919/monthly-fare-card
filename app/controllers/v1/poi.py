f0d6d08a65de9ed1c61ff3b8c46b6eaee5d000d9
fore0919 <gms02294@gmail.com>
2025-03-12 21:13:07 +0900

    fix: add annotation

--- app/controllers/v1/poi.py
+++ app/controllers/v1/poi.py
@@ -104,34 +104,50 @@ class PoiController(ControllerBase):
         """
         graph = defaultdict(list)
         lines_by_line = defaultdict(list)
+        # 노선 id를 키값으로 가진 dict에 station 오브젝트를 list로 할당 {line_id: [StationLine, ...]}
         for sl in station_lines:
             lines_by_line[sl.line_id].append(sl)
         for line_id, sl_list in lines_by_line.items():
+            # 각 노선별 역 순서(sequence) 정렬
             sl_list.sort(key=lambda sl: sl.sequence)
             for i in range(1, len(sl_list)):
-                u = sl_list[i - 1]
-                v = sl_list[i]
+                prev = sl_list[i - 1]
+                curr = sl_list[i]
                 weight = (
-                    v.distance_from_prev
-                    if v.distance_from_prev is not None
+                    curr.distance_from_prev
+                    if curr.distance_from_prev is not None
                     else 0.0
                 )
-                u_node = (u.station_id, u.line_id)
-                v_node = (v.station_id, v.line_id)
-                graph[u_node].append((v_node, weight))
-                graph[v_node].append((u_node, weight))
-
+                prev_node = (
+                    prev.station_id,
+                    prev.line_id,
+                )  # 이전역(노드) 역id, 노선id
+                curr_node = (
+                    curr.station_id,
+                    curr.line_id,
+                )  # 현재역(노드) 역id, 노선id
+                graph[prev_node].append(
+                    (curr_node, weight)
+                )  # {(1,1): [((2,0), 1.0)]}
+                graph[curr_node].append(
+                    (prev_node, weight)
+                )  # {(2,1): [((2,0), 1.0)]}
+        # 같은 역에 속한 다른 노선 간 환승 연결 (환승 비용 추가)
         station_to_nodes = defaultdict(list)
         for sl in station_lines:
             node = (sl.station_id, sl.line_id)
-            station_to_nodes[sl.station_id].append(node)
+            station_to_nodes[sl.station_id].append(
+                node
+            )  # [(1,1),(1,4)] : id가 1인 서울역에 속한 노선이 1호선과 4호선
         for station, nodes in station_to_nodes.items():
             for i in range(len(nodes)):
                 for j in range(i + 1, len(nodes)):
-                    u = nodes[i]
-                    v = nodes[j]
-                    graph[u].append((v, transfer_cost))
-                    graph[v].append((u, transfer_cost))
+                    prev_node = nodes[i]
+                    curr_node = nodes[j]
+                    graph[prev_node].append((curr_node, transfer_cost))
+                    graph[curr_node].append((prev_node, transfer_cost))
+                    # graph = {(2,1) : [((1, 1), Decimal('1.10')), ((3, 1), Decimal('1.00')), ((2, 2), 0.0), ((2, 2), 0.0)] ...}
+                    # id=2(시청역), 1호선 : (id=1(서울역), 1호선) 거리 1.1, (id=3(종각역), 1호선) 거리 1.0, (id=2(시청역), 2호선) 환승비용 0.0...
         return graph
 
     def _dijkstra(
